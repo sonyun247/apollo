a80c0e03753d573b880ce89a0155f3fdab7b0254
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2021-02-22 21:22:10 +0900

    Detail Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,18 @@
-export default () => "Detail";
+import React from "react";
+import { useQuery, gql } from '@apollo/client';
+import { useParams } from "react-router-dom";
+
+const GET_PERSON = gql`
+query getPerson($id: Int!){
+  person(id:$id){id,name,age,gender}
+}
+`;
+
+const Detail = () => {
+    const { id } = useParams();
+    const { loading, error, data } = useQuery(GET_PERSON, { variables: { id: +id } });
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :</p>;
+    return data.person.name;
+}
+export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,5 @@
 import { useQuery, gql } from '@apollo/client';
+import { Link } from "react-router-dom";
 
 const GET_PEOPLE = gql`
 query{
@@ -11,11 +12,13 @@ const Home = () => {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :</p>;
     return data.people.map((person => (
-        <div key={person.id}>
-            <p>
-                {person.name}
-            </p>
-        </div>
+        <Link to={`/${person.id}`}>
+            <div key={person.id}>
+                <p>
+                    {person.name}
+                </p>
+            </div>
+        </Link >
     )))
 }
 

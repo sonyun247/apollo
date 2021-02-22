a6bc651dc2712c638664bb6ce30d63312c96ec04
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2021-02-22 20:49:16 +0900

    Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,23 @@
-export default () => <h1>Home</h1>;
+import { useQuery, gql } from '@apollo/client';
+
+const GET_PEOPLE = gql`
+query{
+  people{id,name}
+}
+`;
+
+const Home = () => {
+    const { loading, error, data } = useQuery(GET_PEOPLE);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :</p>;
+    return data.people.map((person => (
+        <div key={person.id}>
+            <p>
+                {person.name}
+            </p>
+        </div>
+    )))
+}
+
+
+export default Home;

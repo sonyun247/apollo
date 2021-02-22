00b9a3331f68abd40a2c1f0be0a6cd2ed6e4d8ff
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2021-02-22 19:59:26 +0900

    Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
 
 function App() {
   return (
-    <div className="App" />);
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </Router>
+  )
 }
 
 export default App;
